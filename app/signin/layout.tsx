c518d4e3278d017e6ae1aa6aed5af1c3f0212e8d
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-23 01:38:04 +0530

    next.js pratice

---
+++ app/signin/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode;
+}) {
+  return (
+      <div>
+      hi there deto
+      {children}
+      </div>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+
+// import/no-anonymous-default-export
+// eslint-disable-next-line import/no-anonymous-default-export, react/display-name
+export default function(){
+  return (
+    <>
+    hi there bro 
+    </>
+  )
+}
